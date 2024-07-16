9c806e980e08c06237b127e0795a99c72c2c67ae
abaidullah696 <abaidullah696@gmail.com>
2024-07-16 21:18:30 +0500

    Add some more possible codes  for this technique

--- 8-mergetwoarrays.js
+++ 8-mergetwoarrays.js
@@ -41,3 +41,62 @@ return newArr
 }
 
 console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // [1, 2, 3, 4, 5, 6]
+
+
+function mergeSort(arr1, arr2) {
+    console.log("Hello")
+    var mergedArr = arr1.concat(arr2)
+    console.log(mergedArr)
+    for (let i = 0; i < mergedArr.length; i++) {
+       for (let j = 0; j < mergedArr.length; j++) {
+        if (mergedArr[i]<mergedArr[j]) {
+            var temp = mergedArr[i]
+            mergedArr[i] = mergedArr[j]
+            mergedArr[j] = temp
+        }
+        
+       }
+        
+    }
+    return mergedArr
+}
+console.log(mergeSort([1, 3, 5], [2, 4, 6])); // [1, 2, 3, 4, 5, 6]
+
+
+
+
+// optimized code for best case O(n+m) time complexity
+function mergeSortedArrays(arr1, arr2) {
+    let mergedArr = [];
+    let i = 0; // Pointer for arr1
+    let j = 0; // Pointer for arr2
+
+    // Merge elements from both arrays in sorted order
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArr.push(arr1[i]);
+            i++;
+        } else {
+            mergedArr.push(arr2[j]);
+            j++;
+        }
+    }
+
+    // If there are remaining elements in arr1, add them
+    while (i < arr1.length) {
+        mergedArr.push(arr1[i]);
+        i++;
+    }
+
+    // If there are remaining elements in arr2, add them
+    while (j < arr2.length) {
+        mergedArr.push(arr2[j]);
+        j++;
+    }
+
+    return mergedArr;
+}
+
+console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // [1, 2, 3, 4, 5, 6]
+
+
