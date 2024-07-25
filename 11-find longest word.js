89d60e7e7ae7d457a97cc00760d7010b82d93dfd
abaidullah696 <abaidullah696@gmail.com>
2024-07-25 11:23:21 +0500

    Table of 7

---
+++ 11-find longest word.js
@@ -0,0 +1,8 @@
+function findLongestWord(str) {
+    // Your code here
+    console.log(str)
+    var longestWord = 
+    str.split(" ").
+  }
+  console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 6
+  
---
+++ 7table.js
@@ -0,0 +1,9 @@
+var table = 7;
+console.log("Table of " + table)
+
+for (let i = 1; i <= 10; i++) {
+    var result = table * i
+    console.log(table +" * " + i + " = " + result )
+    
+}
+
