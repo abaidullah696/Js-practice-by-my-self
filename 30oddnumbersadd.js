1a090011b0a26b830789bb24b99324bd7d46ee81
abaidullah696 <abaidullah696@gmail.com>
2024-07-26 10:09:25 +0500

    Sum of all odd numbers

--- 30oddnumbersadd.js
+++ 30oddnumbersadd.js
@@ -1,8 +1,9 @@
 function sumOfOdds(startNum, endNum) {
-    
+    let sum = 0
     for (let i = startNum; i <= endNum; i+=2) {
-        console.log(i)
         
+        sum =sum+i
     }
+    return sum
 }
 console.log(sumOfOdds(11, 30))
