da0278637e2a0b99f30acab9e213b6920aca6e7c
abaidullah696 <abaidullah696@gmail.com>
2024-08-01 00:14:37 +0500

    Date in formate

--- getCurentDate.js
+++ getCurentDate.js
@@ -1,5 +1,24 @@
-function GetDate() {
-    const date = new Date()
-    return date
+function getDate() {
+    const today = new Date()
+    
+    const day = String(today.getDate()).padStart(2, '0')
+    const month = String(today.getMonth() + 1).padStart(2, '0')
+    const year = String(today.getFullYear())
+        // return day + month + year
+
+
+        const mm_dd_yyyy = `${month}-${day}-${year}`;
+        const mm_dd_yyyy_slash = `${month}/${day}/${year}`;
+        const dd_mm_yyyy = `${day}-${month}-${year}`;
+        const dd_mm_yyyy_slash = `${day}/${month}/${year}`;
+
+
+
+
+
+        console.log(mm_dd_yyyy);         // Output: mm-dd-yyyy
+        console.log(mm_dd_yyyy_slash);   // Output: mm/dd/yyyy
+        console.log(dd_mm_yyyy);         // Output: dd-mm-yyyy
+        console.log(dd_mm_yyyy_slash);   // Output: dd/mm/yyyy
 }
-console.log(GetDate())
+getDate()
