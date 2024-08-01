1a8c64e9c1be82301a1a756f25d756c73cdbfe55
abaidullah696 <abaidullah696@gmail.com>
2024-08-01 22:10:25 +0500

    Concate 2 strings and remove first char from str

---
+++ concat two str exept 1st word.js
@@ -0,0 +1,8 @@
+function concat2strexept1stword(str1, str2) {
+    const first = str1.slice(1)
+    const second = str2.slice(1)
+
+    return first + str2
+}
+
+console.log(concat2strexept1stword("Hello", "There"))
