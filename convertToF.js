17dbaeed6f5553d5bdba5045cf560f926f896033
abaidullah696 <abaidullah696@gmail.com>
2024-08-18 00:48:57 +0500

    Convert the temprature

---
+++ convertToF.js
@@ -0,0 +1,8 @@
+function convertToF(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+// Example usage:
+console.log(convertToF(0));   // Outputs: 32
+console.log(convertToF(25));  // Outputs: 77
+console.log(convertToF(-30)); // Outputs: -22
