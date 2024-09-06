a2a0523010bd984988918f184a219fc2ce9ac884
abaidullah696 <abaidullah696@gmail.com>
2024-09-07 00:24:33 +0500

    Non repeated values

---
+++ findNonRepeatedValue.js
@@ -0,0 +1,16 @@
+//
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+//You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+function singleNumber(nums) {
+    let result = 0;
+    
+    // XOR all the elements in the array
+    for (let i = 0; i < nums.length; i++) {
+        result ^= nums[i];
+    }
+    
+    return result;
+}
