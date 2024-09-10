0faacdc6aef0910cad6c7ec602848dfb2243e8cd
abaidullah696 <abaidullah696@gmail.com>
2024-09-10 23:13:06 +0500

    Print table from 1 to 10

---
+++ one to 10 table.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 10; i++) {
+    printTable(i)
+    
+}
+function printTable(n) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(n + "*" + i + "=" + n*i)
+        
+    }
+}
